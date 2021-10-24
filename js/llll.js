984814cb06cf6592b7d2ba4f60ed98743a3f1671
diegochillo <72892133+diegochillo@users.noreply.github.com>
2021-10-24 14:52:01 +0200

    Subfolders structure for CSS and JS files

---
+++ js/llll.js
@@ -0,0 +1,7 @@
+/* Loads the articles inside the columns */
+
+function load_articles() {
+  document.getElementById("article1").innerHTML='<object type="text/html" data="articles/article1.html" ></object>';
+  document.getElementById("article2").innerHTML='<object type="text/html" data="articles/article2.html" ></object>';
+  document.getElementById("article3").innerHTML='<object type="text/html" data="articles/article3.html" ></object>';
+}
