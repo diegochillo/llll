4f9885835e222ff854ef69eb8867d587ee5e37df
diegochillo <72892133+diegochillo@users.noreply.github.com>
2021-10-26 19:08:37 +0200

    New way to load external articles, with jQuery

--- js/llll.js
+++ js/llll.js
@@ -1,5 +1,12 @@
 /* Loads the articles inside the columns */
 
+$(document).ready(function(){
+   $('#article1').load("articles/article1.html");
+   $('#article2').load("articles/article2.html");
+   $('#article3').load("articles/article3.html");
+});
+
+
 function load_articles() {
   document.getElementById("article1").innerHTML='<object type="text/html" data="articles/article1.html" ></object>';
   document.getElementById("article2").innerHTML='<object type="text/html" data="articles/article2.html" ></object>';
