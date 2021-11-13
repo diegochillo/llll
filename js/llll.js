69c7c42c004fea95f382e4bacaead9265b0a1079
diegochillo <72892133+diegochillo@users.noreply.github.com>
2021-11-13 19:19:51 +0100

    Second type of metadata: language

--- js/llll.js
+++ js/llll.js
@@ -6,13 +6,12 @@ $(function(){$("#topBar").load("components/topbar.html"); });
 /* Loads the articles inside the columns using JQuery*/
 
 $(document).ready(function(){
-   $('#article1_1').load("articles/article1_1.html", function() { getMetadata("#article1_1","#metaData1_1"); } );
-   $('#article1_2').load("articles/article1_2.html", function() { getMetadata("#article1_2","#metaData1_2"); } );
-   $('#article1_3').load("articles/article1_3.html", function() { getMetadata("#article1_3","#metaData1_3"); } );
-   $('#article2_1').load("articles/article2_1.html", function() { getMetadata("#article2_1","#metaData2_1"); } );
-   $('#article2_2').load("articles/article2_2.html", function() { getMetadata("#article2_2","#metaData2_2"); } );
-   $('#article2_3').load("articles/article2_3.html", function() { getMetadata("#article2_3","#metaData2_3"); } );
-
+   $('#article1_1').load("articles/article1_1.html", function() { getMetadata(1,1); } ); //"#article1_1","#metaData1_1"
+   $('#article1_2').load("articles/article1_2.html", function() { getMetadata(1,2); } );
+   $('#article1_3').load("articles/article1_3.html", function() { getMetadata(1,3); } );
+   $('#article2_1').load("articles/article2_1.html", function() { getMetadata(2,1); } );
+   $('#article2_2').load("articles/article2_2.html", function() { getMetadata(2,2); } );
+   $('#article2_3').load("articles/article2_3.html", function() { getMetadata(2,3); } );
 });
 
 
@@ -23,16 +22,20 @@ function switchToSS (nodeE,nodeD1,nodeD2,nodeD3) {
   nodeD1.media = 'none';
   nodeD2.media = 'none';
   nodeD3.media = 'none';
-  /* nodeD2.media = 'none'; */
 }
 
 
 
 // Gets the list of metadata and shows it in the metaData box (only for .person class at the moment)
-function getMetadata(elementReadId,elementMetaId) {
-  // Loads metadata
+function getMetadata(nIssue,nArticle) {
 
-    var dataList = $(elementReadId + " .person").map(function() {
+    var elementReadId = "#article" + nIssue + "_" + nArticle;
+    var metaType = "";
+
+    // PERSONS
+    metaType = "person";
+    elementMetaId="#" + metaType + "s" + nIssue + "_" + nArticle;
+    var dataList = $(elementReadId + " ." + metaType).map(function() {
         return $(this).data("label");
     }).get();
 
@@ -42,7 +45,24 @@ function getMetadata(elementReadId,elementMetaId) {
     // Cycles over found elements and shows checkboxes
     cntr=1;
     for (let md of dataListU) {
-      $(elementMetaId).append('<input type="checkbox" class="metaCheck" id="metaCheck-' + cntr + '" value="1" onclick="showMeta(\''+elementReadId+'\',\'' + md + '\',this)"> ' + md + '<br/>');
+      $(elementMetaId).append('<input type="checkbox" class="metaCheck" id="metaCheck-' + cntr + '" value="1" onclick="showMeta(\''+elementReadId+'\',\'' + md + '\',this,\'' + metaType + '\')"> ' + md + '<br/>');
+      cntr=cntr+1;
+    }
+
+    // PERSONS
+    metaType = "language";
+    elementMetaId="#" + metaType + "s" + nIssue + "_" + nArticle;
+    var dataList = $(elementReadId + " ." + metaType).map(function() {
+        return $(this).data("label");
+    }).get();
+
+    var dataListU=[... new Set(dataList)];
+    // $(elementMetaId).html(dataListU.join("<br>"));
+
+    // Cycles over found elements and shows checkboxes
+    cntr=1;
+    for (let md of dataListU) {
+      $(elementMetaId).append('<input type="checkbox" class="metaCheck" id="metaCheck-' + cntr + '" value="1" onclick="showMeta(\''+elementReadId+'\',\'' + md + '\',this,\'' + metaType + '\')"> ' + md + '<br/>');
       cntr=cntr+1;
     }
 
@@ -50,17 +70,21 @@ function getMetadata(elementReadId,elementMetaId) {
 
 
 // Highlights the text corresponding to the selected checkbox (only for .person class at the moment)
-function showMeta(elementReadId,label,chkbx) {
-    thisTimeColor = "#" + Math.floor((Math.random() * 16777215) + 1).toString(16);
-    $(elementReadId + " .person").each(function(index) {
+function showMeta(elementReadId,label,chkbx,metaType) {
+
+    var thisTimeColor = "#" + Math.floor((Math.random() * 16777215) + 1).toString(16);
+    var found=false;
+
+    $(elementReadId + " ." + metaType).each(function(index) {
       if ($(this).text() == label) {
         if (chkbx.checked) {
           $(this).css("background-color", thisTimeColor);
-
-            $(elementReadId).animate({
-                scrollTop: $(elementReadId).scrollTop() + $(this).offset().top - 400
-            }, 1000);
-
+            if (!found) {
+              $(elementReadId).animate({
+                  scrollTop: $(elementReadId).scrollTop() + $(this).offset().top - 400
+              }, 1000);
+              found=true;
+            }
         } else {
           $(this).css("background-color", "");
         }
