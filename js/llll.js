2c52bc74bc58afc271adcbc079c66d864e0ec590
diegochillo <72892133+diegochillo@users.noreply.github.com>
2021-11-27 17:41:18 +0100

    Added tabs inside metadata selector boxes

--- js/llll.js
+++ js/llll.js
@@ -6,12 +6,12 @@ $(function(){$("#topBar").load("components/topbar.html"); });
 /* Loads the articles inside the columns using JQuery*/
 
 $(document).ready(function(){
-   $('#article1_1').load("articles/article1_1.html", function() { getMetadata(1,1,["person","language"]); } ); //"#article1_1","#metaData1_1"
-   $('#article1_2').load("articles/article1_2.html", function() { getMetadata(1,2,["person","language"]); } );
-   $('#article1_3').load("articles/article1_3.html", function() { getMetadata(1,3,["person","language"]); } );
-   $('#article2_1').load("articles/article2_1.html", function() { getMetadata(2,1,["person","language"]); } );
-   $('#article2_2').load("articles/article2_2.html", function() { getMetadata(2,2,["person","language"]); } );
-   $('#article2_3').load("articles/article2_3.html", function() { getMetadata(2,3,["person","language"]); } );
+   $('#article1_1').load("articles/article1_1.html", function() { getMetadata(1,1,["person","place","date","keyword"]); } ); //"#article1_1","#metaData1_1"
+   $('#article1_2').load("articles/article1_2.html", function() { getMetadata(1,2,["person","place","date","keyword"]); } );
+   $('#article1_3').load("articles/article1_3.html", function() { getMetadata(1,3,["person","place","date","keyword"]); } );
+   $('#article2_1').load("articles/article2_1.html", function() { getMetadata(2,1,["person","language","place","date","keyword"]); } );
+   $('#article2_2').load("articles/article2_2.html", function() { getMetadata(2,2,["person","language","place","date","keyword"]); } );
+   $('#article2_3').load("articles/article2_3.html", function() { getMetadata(2,3,["person","language","place","date","keyword"]); } );
 });
 
 
@@ -33,27 +33,53 @@ function cleanUpSS() {
 
 
 
-// Gets the list of metadata and shows it in the metaData box (only for .person class at the moment)
+// Gets the list of metadata and shows it in the metaData box tabs
 function getMetadata(nIssue,nArticle,metaList) {
 
-    var elementReadId = "#article" + nIssue + "_" + nArticle;
+    var suffix = nIssue + "_" + nArticle;
+    var elementReadId = "#article" + suffix;
+    var elementMetaTabs = "#tabs" +  suffix;
+    var elementMetaData = "#metaData" + suffix;
+    var elementTabContent = "#content" + suffix;
 
+    // Creates the tab menu inside the metadata selector box
+    var ariasel=true;
+    var tabactive='active';
     for (const metaType of metaList) {
-      //metaType = "person";
+      $(elementMetaTabs).append('<li class="nav-item waves-effect waves-light" role="presentation"><a class="nav-link ' + tabactive + '" id="' + metaType + '-tab' + suffix + '" data-toggle="tab" href="#' + metaType + suffix + '" type="button" role="tab" aria-controls="' + metaType + '" aria-selected="' + ariasel + '">' + metaType + '</a></li>');
+      ariasel='false';
+      tabactive='';
+    }
+
+    var tabactive='active';
+    var mystring='';
+    for (const metaType of metaList) {  // For each type of metadata
+
+      mystring='<div class="tab-pane ' + tabactive + '" id="' + metaType + suffix + '" role="tabpanel" aria-labelledby="' + metaType + '-tab' + suffix+'">';
+      // console.log(mystring);
+
       elementMetaId="#" + metaType + "s" + nIssue + "_" + nArticle;
       var dataList = $(elementReadId + " ." + metaType).map(function() {
           return $(this).data("label");
       }).get();
 
       var dataListU=[... new Set(dataList)];
-      // $(elementMetaId).html(dataListU.join("<br>"));
 
       // Cycles over found elements and shows checkboxes
       cntr=1;
       for (let md of dataListU) {
-        $(elementMetaId).append('<input type="checkbox" class="metaCheck" id="metaCheck-' + cntr + '" value="1" onclick="showMeta(\''+elementReadId+'\',\'' + md + '\',this,\'' + metaType + '\')"> ' + md + '<br/>');
+        // $(elementMetaId).append('<input type="checkbox" class="metaCheck" id="metaCheck-' + cntr + '" value="1" onclick="showMeta(\''+elementReadId+'\',\'' + md + '\',this,\'' + metaType + '\')"> ' + md + '<br/>');
+        mystring+='<input type="checkbox" class="metaCheck" id="metaCheck-' + cntr + '" value="1" onclick="showMeta(\''+elementReadId+'\',\'' + md + '\',this,\'' + metaType + '\')"> ' + md + '<br/>';
         cntr=cntr+1;
       }
+
+      mystring+='</div>';
+
+      $(elementTabContent).append(mystring);
+      tabactive='';
+
+      // console.log($("#metaData2_1").html());
+
     }
 
 }
